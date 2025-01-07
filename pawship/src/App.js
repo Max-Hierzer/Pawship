3d633cec9385ac8e20fa49a5535756ba8f4a76ed
gernotfasching <gernotfasch@gmail.com>
2025-01-07 20:10:19 +0100

    edited path

--- pawship/src/App.js
+++ pawship/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Homepage from './pages/Homepage/Homepage';
-import Contact from './pages/Contact/Contact';
-import About from './pages/About/About';
-import Animals from './pages/Animals/Animals';
-import Donations from './pages/Donations/Donations';
+import Homepage from './pages/Homepage/Homepage.js';
+import Contact from './pages/Contact/Contact.js';
+import About from './pages/About/About.js';
+import Animals from './pages/Animals/Animals.js';
+import Donations from './pages/Donations/Donations.js';
 import './styles/App.css';
 
 function App() {
