d70b10b88731c33656c8989767eab9a4f827d7e7
Tobias <tobias.prasser@gmail.com>
2025-01-08 21:25:40 +0100

    homepage text hinzugefuegt

--- pawship/src/pages/Homepage/Homepage.js
+++ pawship/src/pages/Homepage/Homepage.js
@@ -32,7 +32,41 @@ function Homepage() {
           </div>
         </div>
       </Carousel>
-      <h1>Finde deinen Pawfekten companion!</h1>
+      <div className="main">
+        <br />
+        <h1>Pawship</h1>
+        <p>Wir vermitteln nicht nur Tiere – wir schaffen Verbindungen fürs Leben. Unsere Mission ist es, liebevollen Tieren eine zweite Chance zu geben und Familien mit ihrem perfekten tierischen Begleiter zusammenzubringen.</p>
+        <br />
+        <h1>Warum Pawship?</h1>
+        <ul>
+          <li>Für die Tiere: Jedes Tier verdient ein Zuhause, in dem es geliebt und geschätzt wird.</li>
+          <li>Für dich: Tiere bereichern unser Leben und schenken bedingungslose Liebe.</li>
+          <li>Mach den Unterschied: Gib einem Tier eine zweite Chance.</li>
+        </ul>
+        <br />
+        <br />
+        <h1>Mach den Unterschied!</h1>
+        <p>Durch Adoption gibst du einem Tier nicht nur ein Zuhause, sondern auch die Chance auf ein glückliches Leben. Schau dir unsere Tiere an und finde deinen neuen besten Freund!</p>
+        <br />
+        <h1>So funktioniert’s:</h1>
+        <ol className="styled-list">
+          <li>
+            <strong>Stöbern:</strong> Sieh dir die verfügbaren Tiere auf unserer Seite an.
+          </li>
+          <li>
+            <strong>Verlieben:</strong> Finde das Tier, das perfekt zu dir passt.
+          </li>
+          <li>
+            <strong>Adoptieren:</strong> Wir helfen dir bei jedem Schritt, um sicherzustellen, dass dein neuer Begleiter sich bei dir wohlfühlt.
+          </li>
+        </ol>
+        <br />
+        <br />
+        <h1>Werde Teil von Pawship!</h1>
+        <p>Gemeinsam machen wir die Welt ein wenig besser – ein Tier nach dem anderen.</p>
+        <br />
+        <br />
+      </div>
     </div>
   );
 }
