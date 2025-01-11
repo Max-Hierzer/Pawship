9c97bfb3407f71834ae7a756b63b4a6d3d7393ba
alexanderleitner0 <alexander.leitner0@bulme.at>
2025-01-11 11:29:07 +0100

    added simple about page

--- pawship/src/pages/About/about.js
+++ pawship/src/pages/About/about.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import './about.css'; 
+import './about.css';
 
-function About() {
+const About = () => {
+  const team = [
+    { title: 'Leitung', name: 'Johanna Theissing' },
+    { title: 'Obmann', name: 'Gernot Fasching' },
+    { title: 'Kassier', name: 'Alexander Leitner' },
+    { title: 'Tierpfleger/innen', name: 'Nevena Popovic, Tobias Prasser und Philipp Müller' },
+  ];
+
   return (
-    <div className="About">
-      <h1>Das sind wir!</h1>
+    <div className="about">
+      <h1 className="about-title">Unser Team</h1>
+      
+      <div className="team-list">
+        {team.map(({ title, name }) => (
+          <div key={title} className="team-entry">
+            <span className="role">{title}</span>
+            <span className="name">{name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default About;
