56d33ae4fbb0c1909887a5303f2ab29599c6cd19
Nevena Popovic <nevena.popovic@bulme.at>
2025-01-14 22:12:39 +0100

    donation page v2

--- pawship/src/pages/Donations/donations.js
+++ pawship/src/pages/Donations/donations.js
@@ -5,28 +5,70 @@ function Donations() {
   return (
     <div className="Donations">
       <h1>Wir freuen uns auf deine Spende!</h1>
+
       <form>
-        <div className="form-group">
-          <label htmlFor="vorname">Vorname:</label>
-          <input type="text" id="vorname" name="vorname" />
+        <div className="donation-box">
+          <div className="options">
+            <label>
+              <input type="radio" name="option" value="einmalig" />
+              einmalig
+            </label>
+            <label>
+              <input type="radio" name="option" value="monatlich" />
+              monatlich
+            </label>
+          </div>
+
+          <div className="prices">
+            <label className="custom-radio">
+              <input type="radio" name="amount" value="5" id="amount-5" />
+              <span className="radio-box">5€</span>
+            </label>
+            <label className="custom-radio">
+              <input type="radio" name="amount" value="10" id="amount-10" />
+              <span className="radio-box">10€</span>
+            </label>
+            <label className="custom-radio">
+              <input type="radio" name="amount" value="20" id="amount-20" />
+              <span className="radio-box">20€</span>
+            </label>
+            <label className="custom-radio">
+              <input type="radio" name="amount" value="50" id="amount-50" />
+              <span className="radio-box">50€</span>
+            </label>
+          </div>
+
+          <div className="oder">oder</div>
+
+          <div className="self-price">
+            <input
+              type="number"
+              id="custom-spende"
+              name="custom-spende"
+              min="1"
+              placeholder="Eigener Betrag"
+            />
+          </div>
         </div>
 
-        <div className="form-group">
-          <label htmlFor="nachname">Nachname:</label>
-          <input type="text" id="nachname" name="nachname" />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="spende">Spende:</label>
-          <select id="spende" name="spende">
-            <option value="">Bitte wählen</option>
-            <option value="5">5€</option>
-            <option value="10">10€</option>
-            <option value="20">20€</option>
-            <option value="50">50€</option>
-            <option value="100">100€</option>
-          </select>
-          
+        <div className='data'>
+          <h1>Meine Daten</h1>
+          <div className='info'>
+            <select name="gender" id="gender">
+              <option value="" disabled selected>Anrede</option>
+              <option value="frau">Frau</option>
+              <option value="herr">Herr</option>
+              <option value="firma">Firma</option>
+            </select>
+            <input type='text' placeholder='Vorname' />
+            <input type='text' placeholder='Nachname' />
+            <input type='text' placeholder='Anschrift' />
+            <input type='text' placeholder='PLZ' />
+            <input type='text' placeholder='Ort' />
+            <input type='text' placeholder='Land' />
+            <input type='text' placeholder='Telefon' />
+            <input type='text' placeholder='Email' />
+          </div>
         </div>
 
         <input type="submit" value="Spenden" />
