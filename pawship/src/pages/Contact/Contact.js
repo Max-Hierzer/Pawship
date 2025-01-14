952069304b3e64d66395166f10d3c6cbf7237373
alexanderleitner0 <alexander.leitner0@bulme.at>
2025-01-14 13:46:01 +0100

    added workin contact form

--- pawship/src/pages/Contact/Contact.js
+++ pawship/src/pages/Contact/Contact.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './Contact.css';
+import { useFormData } from 'herotofu-react';
 
 const Contact = () => {
+  const { formState, getFormSubmitHandler } = useFormData(
+    'https://public.herotofu.com/v1/91318510-d26b-11ef-9a5c-d31370001c66'
+  );
+
   const opentime = [
     { tag: 'Montag', zeit: '09:00 - 18:00' },
     { tag: 'Dienstag', zeit: 'Geschlossen' },
@@ -9,8 +14,9 @@ const Contact = () => {
     { tag: 'Donnerstag', zeit: '09:00 - 18:00' },
     { tag: 'Freitag', zeit: '09:00 - 15:00' },
     { tag: 'Samstag', zeit: '11:00 - 14:00' },
-    { tag: 'Sonntag', zeit: 'Geschlossen' }
+    { tag: 'Sonntag', zeit: 'Geschlossen' },
   ];
+
 
   return (
     <div className="contact">
@@ -30,6 +36,33 @@ const Contact = () => {
 
       <div className="contact-info">
         <h2>Kontakt</h2>
+        <form onSubmit={getFormSubmitHandler()} acceptCharset="UTF-8">
+          <label>
+            <div>
+              <input type="text" placeholder="Dein Name" name="name" required />
+            </div>
+          </label>
+          <label>
+            <div>
+              <input type="text" placeholder="E-Mail-Adresse" name="email" required />
+            </div>
+          </label>
+          <label>
+            <div>
+              <input type="text" placeholder="Betreff" name="betreff" required />
+            </div>
+          </label>
+          <label>
+            <div>
+              <textarea placeholder="Nachricht" name="nachricht" required />
+            </div>
+          </label>
+          <div>
+            <input type="submit" value="Absenden" />
+          </div>
+        </form>
+
+        <h2>Andere Kontaktmöglichkeiten</h2>
         <div className="contact-details">
           <p>
             <strong>Adresse:</strong> Ibererstraße 15-21, 8051 Graz
