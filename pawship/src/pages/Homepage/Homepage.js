96d405a35f19aa35bd79e34844bbc34baebfbd48
Tobias <tobias.prasser@gmail.com>
2025-01-14 12:33:59 +0100

    adopt now button added

--- pawship/src/pages/Homepage/Homepage.js
+++ pawship/src/pages/Homepage/Homepage.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { useNavigate } from 'react-router-dom';
 import banner1 from "../../images/banner1.jpg";
 import banner2 from "../../images/banner2.jpg";
 import banner3 from "../../images/banner3.jpg";
 import './Homepage.css';
 
+
 function Homepage() {
+
+  const navigate = useNavigate();
+
+  const handleAdoptNowClick = () => {
+    navigate('/tiere');
+  };
+
+
   return (
     <div className="homepage">
       <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false}>
@@ -47,6 +57,10 @@ function Homepage() {
         <br />
         <h1>Mach den Unterschied!</h1>
         <p>Durch Adoption gibst du einem Tier nicht nur ein Zuhause, sondern auch die Chance auf ein glückliches Leben. Schau dir unsere Tiere an und finde deinen neuen besten Freund!</p>
+        <button onClick={handleAdoptNowClick} className="adopt-button">
+          Adopt Now
+        </button>
+        <br />
         <br />
         <h1>So funktioniert’s:</h1>
         <ol className="styled-list">
