b10b06503cd063cca8e52c0deee7826b3990e341
Max Hierzer <max.hierzer@bulme.at>
2025-01-14 13:34:17 +0100

    .

--- pawship/src/pages/Animals/animals.js
+++ pawship/src/pages/Animals/animals.js
@@ -7,22 +7,22 @@ import './animals.css';
 import AnimalTemplate from './animalTemplate.js';
 
 
-const animalDataDog = [
-  {
-    name: 'Laika', images: [
-      require('../../images/Laika/IMG_20241101_150555.jpg'),
-      require('../../images/Laika/IMG_20220720_151314.jpg')
-    ],
-  },
-  {
-    name: 'Mia', images: [
-      require('../../images/Mia/7A60B11E-7613-4C01-BB46-A76FE5ABD2EA_1_105_c.jpeg'),
-      require('../../images/Mia/7147BC44-8A9E-4B98-8C39-EE97BB89533E_1_105_c.jpeg')
-    ],
-  }
-  // { name: 'JJ', image: require('../../images/JJ/Foto 29.10.23, 10 46 30.jpg') },
-  // { name: 'Sammie', image: require('../../images/Sammie/Foto 29.10.23, 10 59 14.jpg') }
-];
+// const animalDataDog = [
+//   {
+//     name: 'Laika', images: [
+//       require('../../images/Laika/IMG_20241101_150555.jpg'),
+//       require('../../images/Laika/IMG_20220720_151314.jpg')
+//     ],
+//   },
+//   {
+//     name: 'Mia', images: [
+//       require('../../images/Mia/7A60B11E-7613-4C01-BB46-A76FE5ABD2EA_1_105_c.jpeg'),
+//       require('../../images/Mia/7147BC44-8A9E-4B98-8C39-EE97BB89533E_1_105_c.jpeg')
+//     ],
+//   }
+//   // { name: 'JJ', image: require('../../images/JJ/Foto 29.10.23, 10 46 30.jpg') },
+//   // { name: 'Sammie', image: require('../../images/Sammie/Foto 29.10.23, 10 59 14.jpg') }
+// ];
 
 // const animalDataCat = [
 //   { name: 'Simba', image: require('../../images/Simba/Foto 25.08.24, 11 22 43.jpg') },
@@ -31,40 +31,11 @@ const animalDataDog = [
 
 
 
-function AnimalSlideshow({ images }) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
-  return (
-    <Slider {...settings}>
-      {images.map((image, index) => (
-        <div key={index}>
-          <img src={image} alt={`slide-${index}`} className="animal-slide-image" />
-        </div>
-      ))}
-    </Slider>
-  );
-}
-
-
 function Animals() {
   return (
     <div className="Animals">
       <div className="Dogs">
         <h1>Unsere Hunde</h1>
-        <div className="grid-container">
-          {animalDataDog.map((animal) => (
-            <Link to={`/animal/${animal.name.toLowerCase()}`} key={animal.name}>
-              <AnimalSlideshow images={animal.images} />
-              <p>{animal.name}</p>
-            </Link>
-          ))}
-        </div>
       </div>
       <div className="Cats">
         <h1>Unsere Katzen</h1>
