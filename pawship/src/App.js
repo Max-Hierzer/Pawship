5a4554264b09370be6f283a430874a8abc763d90
Max Hierzer <max.hierzer@bulme.at>
2025-01-18 14:00:21 +0100

    Changed animals a bit to get the data from app so we can use data also in animaltemplate

--- pawship/src/App.js
+++ pawship/src/App.js
@@ -6,10 +6,71 @@ import Homepage from './pages/Homepage/Homepage.js';
 import Contact from './pages/Contact/Contact.js';
 import About from './pages/About/about.js';
 import Animals from './pages/Animals/animals.js';
+import AnimalTemplate from './pages/Animals/animalTemplate';
 import Donations from './pages/Donations/donations.js';
 import './styles/App.css';
 
+
 function App() {
+  const animalData = {
+    dogs: [
+      {
+        name: 'Laika',
+        images: [
+          'Laika1.jpg',
+          'Laika2.jpg',
+          'Laika3.jpg',
+          'Laika4.jpg',
+          'Laika5.jpg'
+        ]
+      },
+      {
+        name: 'Mia',
+        images: [
+          'Mia1.jpeg',
+          'Mia2.jpeg',
+          'Mia3.jpeg',
+          'Mia4.jpeg'
+        ]
+      },
+      {
+        name: 'JJ',
+        images: [
+          'JJ1.jpg',
+          'JJ2.jpg',
+          'JJ3.jpg',
+          'JJ4.jpg',
+          'JJ5.jpg',
+          'JJ6.jpg'
+        ]
+      },
+      {
+        name: 'Sammie',
+        images: [
+          'Sammie1.jpg',
+          'Sammie2.jpg',
+          'Sammie2.jpg'
+        ]
+      },
+    ],
+    cats: [
+      {
+        name: 'Simba',
+        images: [
+          'Simba1.jpg',
+          'Simba2.jpg'
+        ]
+      },
+      {
+        name: 'Nala',
+        images: [
+          'Nala1.jpg',
+          'Nala2.jpg'
+        ]
+      }
+    ]
+  };
+
   return (
     <BrowserRouter>
       <div>
@@ -18,7 +79,8 @@ function App() {
           <Routes> 
             <Route path="/" element={<Homepage />} /> 
             <Route path="/contact" element={<Contact />} /> 
-            <Route path="/tiere" element={<Animals />} /> 
+            <Route path="/tiere" element={<Animals animalData = {animalData}/>} />
+            <Route path="/tiere/Laika" element={<AnimalTemplate name = {'Laika'} age = {5} nature = {'happy'} images = {['Laika1.jpg', 'Laika2.jpg', 'Laika3.jpg', 'Laika4.jpg', 'Laika5.jpg']} />} />
             <Route path="/spenden" element={<Donations />} /> 
             <Route path="/about" element={<About />} /> 
           </Routes>
--- pawship/src/pages/Animals/animals.js
+++ pawship/src/pages/Animals/animals.js
@@ -4,75 +4,17 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './animals.css';
 
-const animalData = {
-  dogs: [
-    {
-      name: 'Laika',
-      images: [
-        'Laika1.jpg',
-        'Laika2.jpg',
-        'Laika3.jpg',
-        'Laika4.jpg',
-        'Laika5.jpg'
-      ]
-    },
-    {
-      name: 'Mia',
-      images: [
-        'Mia1.jpeg',
-        'Mia2.jpeg',
-        'Mia3.jpeg',
-        'Mia4.jpeg'
-      ]
-    },
-    {
-      name: 'JJ',
-      images: [
-        'JJ1.jpg',
-        'JJ2.jpg',
-        'JJ3.jpg',
-        'JJ4.jpg',
-        'JJ5.jpg',
-        'JJ6.jpg'
-      ]
-    },
-    {
-      name: 'Sammie',
-      images: [
-        'Sammie1.jpg',
-        'Sammie2.jpg',
-        'Sammie2.jpg'
-      ]
-    },
-  ],
-  cats: [
-    {
-      name: 'Simba',
-      images: [
-        'Simba1.jpg',
-        'Simba2.jpg'
-      ]
-    },
-    {
-      name: 'Nala',
-      images: [
-        'Nala1.jpg',
-        'Nala2.jpg'
-      ]
-    }
-  ]
-};
 
-function Animals() {
+function Animals(animalData) {
   const basePath = `${process.env.PUBLIC_URL}/images/`;
-
+  console.log(animalData)
   return (
     <div className="Animals">
       <div className="Dogs">
         <h1>Unsere Hunde</h1>
         <div className="grid-container">
-          {animalData.dogs.map((animal) => (
-            <Link to={`/animal/${animal.name.toLowerCase()}`} key={animal.name} className="carousel-link">
+          {animalData.animalData.dogs.map((animal) => (
+            <Link to={`/tiere/${animal.name.toLowerCase()}`} key={animal.name} className="carousel-link">
               <Carousel infiniteLoop showThumbs={false} showStatus={false} className="carousel-small">
                 {animal.images.map((image, index) => (
                   <div key={index}>
@@ -92,8 +34,8 @@ function Animals() {
       <div className="Cats">
         <h1>Unsere Katzen</h1>
         <div className="grid-container">
-          {animalData.cats.map((animal) => (
-            <Link to={`/animal/${animal.name.toLowerCase()}`} key={animal.name} className="carousel-link">
+          {animalData.animalData.cats.map((animal) => (
+            <Link to={`/tiere/${animal.name.toLowerCase()}`} key={animal.name} className="carousel-link">
               <Carousel infiniteLoop showThumbs={false} showStatus={false} className="carousel-small">
                 {animal.images.map((image, index) => (
                   <div key={index}>
