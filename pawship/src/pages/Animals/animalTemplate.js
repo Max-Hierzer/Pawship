7ebbb673f412e54c2e8627230fb9f9350449e1c5
Max Hierzer <max.hierzer@bulme.at>
2025-01-18 16:29:49 +0100

    css

--- pawship/src/pages/Animals/animalTemplate.js
+++ pawship/src/pages/Animals/animalTemplate.js
@@ -12,34 +12,32 @@ function AnimalTemplate(data) {
                 {data.images.map((i) => {
                     const picLink = linkToImages + i;
                     return (
-                        <div>
-                            <img src={picLink} alt={i} />
+                        <div key={i}>
+                        <img src={picLink} alt={i} />
                         </div>
                     );
-                })
-
-                }
+                })}
             </Carousel>
-            <div id='steckbrief'>
+            <div id="steckbrief">
                 <h1>{data.name}</h1>
                 <h2>Steckbrief</h2>
                 <div className="info">
                     <h3>
-                        <span className="label">Alter:</span> <span className="value">{data.age}</span>
+                    <span className="label">Alter:</span> <span className="value">{data.age}</span>
                     </h3>
                     <h3>
-                        <span className="label">Geschlecht:</span> <span className="value">{data.gender}</span>
+                    <span className="label">Geschlecht:</span> <span className="value">{data.gender}</span>
                     </h3>
                 </div>
-
-                <h3><span className='label'>Natur:</span></h3>
+                <h3><span className="label">Natur:</span></h3>
                 <ul>
-                    {data.nature.map((nat) => (
-                        <li key={data.name}>{nat}</li>
-                    ))}
+                {data.nature.map((nat, index) => (
+                    <li key={index}>{nat}</li>
+                ))}
                 </ul>
             </div>
         </div>
+
     )
 }
 
