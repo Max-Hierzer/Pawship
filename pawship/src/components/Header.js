9c92513826ae27296b50d0af1d42eaa580dc7629
Tobias <tobias.prasser@gmail.com>
2025-01-19 17:49:17 +0100

    spenden button removed

--- pawship/src/components/Header.js
+++ pawship/src/components/Header.js
@@ -11,7 +11,6 @@ function Header() {
                     <img src={logo} alt="Tierheim Logo" />
                 </a>
                 <Navigation />
-                <button className="donate-button">Spenden</button>
             </div>
         </header>
     );
